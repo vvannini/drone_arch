c007c33bf9472e47aaac71ca6598e53f326db234
gustavo-moura <gus.moura@outlook.com>
2019-10-15 21:06:52 -0300

    added gui

--- drone_ws/src/planners/scripts/genetic_pathplanning/ros_genetic_server.py
+++ drone_ws/src/planners/scripts/genetic_pathplanning/ros_genetic_server.py
@@ -11,98 +11,116 @@ from data_definitions import Mapa, CartesianPoint
 
 from file_manipulation import read_mission
 
+
+## Interface Gráfica
+from visualization import plot_map
+## /Interface Gráfica
+
 def build_sample_map():
-	# Função para criar um mapa de teste
+    # Função para criar um mapa de teste
 
-	wp_ori = CartesianPoint(10.0, 10.0)
-	wp_des = CartesianPoint(40.0, 10.0)
+    wp_ori = CartesianPoint(10.0, 10.0)
+    wp_des = CartesianPoint(40.0, 10.0)
 
-	verts = [
-	    (20.0,  5.0), # left,  bottom
-	    (20.0, 15.5), # left,  top
-	    (30.0, 15.5), # right, top
-	    (30.0,  5.0), # right, bottom
-	    (20.0,  5.0), # ignored
-	]
-	verts = [CartesianPoint(v[0], v[1]) for v in verts]
+    verts = [
+        (20.0,  5.0), # left,  bottom
+        (20.0, 15.5), # left,  top
+        (30.0, 15.5), # right, top
+        (30.0,  5.0), # right, bottom
+        (20.0,  5.0), # ignored
+    ]
+    verts = [CartesianPoint(v[0], v[1]) for v in verts]
 
-	mapa = Mapa(wp_ori, wp_des, [verts])
+    mapa = Mapa(wp_ori, wp_des, [verts])
 
-	return mapa
+    return mapa
 
 
 def run_genetic(req):
-	
-	## ENTRADA
+    
+    ## ENTRADA
 
-	# Parâmetros recebidos (arquivo .srv)
-	origin_lat       = req.origin_lat
-	origin_long      = req.origin_long
-	origin_alt       = req.origin_alt
-	destination_lat  = req.destination_lat
-	destination_long = req.destination_long
-	destination_alt  = req.destination_alt
-	missao_id        = req.mission_id
+    # Parâmetros recebidos (arquivo .srv)
+    origin_lat       = req.origin_lat
+    origin_long      = req.origin_long
+    origin_alt       = req.origin_alt
+    destination_lat  = req.destination_lat
+    destination_long = req.destination_long
+    destination_alt  = req.destination_alt
+    missao_id        = req.mission_id
 
-	# Leitura do arquvio em DATA
-	geo_home, _, _, areas_n = read_mapa('~/Data/mapa.json', mapa_id)
+    # Leitura do arquvio em DATA
+    geo_home, _, _, areas_n = read_mapa('~/Data/mapa.json', mapa_id)
 
-	cart_origin      = geo_to_cart(GeoPoint(origin_lat, origin_long, origin_alt), geo_home)
-	cart_destination = geo_to_cart(GeoPoint(destination_lat, destination_long, destination_alt), geo_home)
+    cart_origin      = geo_to_cart(GeoPoint(origin_lat, origin_long, origin_alt), geo_home)
+    cart_destination = geo_to_cart(GeoPoint(destination_lat, destination_long, destination_alt), geo_home)
 
 
     mapa = Mapa(cart_origin, cart_destination, areas_n, inflation_rate=0.1)
 
 
 
-	## EXECUÇÃO DO AG
+    ## EXECUÇÃO DO AG
 
-	ag = Genetic(Subject, mapa,
-	        taxa_cross=1.0,
-	        population_size=80,
-	        C_d=1000,
-	        C_obs=10000,
-	        C_con=10,
-	        C_cur=0,
-	        C_t=0,
-	        max_exec_time=60
-	)
+    ag = Genetic(Subject, mapa,
+        taxa_cross=1.0,
+        population_size=80,
+        C_d=1000,
+        C_obs=10000,
+        C_con=10,
+        C_cur=0,
+        C_t=0,
+        max_exec_time=1,
+        T_max=20,
+        px0=cart_origin.x,
+        py0=cart_origin.y
+    )
 
-	best = ag.run(info=True)
+    best = ag.run(info=True)
 
-	# Melhor rota encontrada : WPs em cartesiano
-	cart_points = best.get_route()
+    # Melhor rota encontrada : WPs em cartesiano
+    cart_points = best.get_route()
 
-	# Melhor rota encontrada : WPs em geográfico
-	geo_points = [ Conversor.list_cart_to_geo(cart_points, geo_home) ]
-
-
-
-	## SAÍDA
-
-	output_filename = '~/Mission/path_from_ga_output.wp'
-	write_mavros(output_filename, geo_points)
+    # Melhor rota encontrada : WPs em geográfico
+    geo_points = [ Conversor.list_cart_to_geo(cart_points, geo_home) ]
 
 
-	return GA_PlannerResponse(output_filename)
+    ## SAÍDA
+
+    ## Interface Gráfica
+    plot_map(
+    mapa.areas_n_inf, 
+    ['n' for _ in range(len(mapa.areas_n_inf))], 
+    cart_origin, 
+    cart_destination, 
+    best.get_route(),
+    None
+    )
+    ## /Interface Gráfica
+
+    output_filename = '~/Mission/path_from_ga_output.wp'
+    write_mavros(output_filename, geo_points)
+
+
+    return GA_PlannerResponse(output_filename)
 
 
 def genetic_server():
-	rospy.init_node('genetic_server')
-	s = rospy.Service('genetic', GA_Planner, run_genetic)
-	print ("Running Genetic Algorithm to Path-Planning")
-	rospy.spin()
+    rospy.init_node('genetic_server')
+    s = rospy.Service('genetic', GA_Planner, run_genetic)
+    print ("Running Genetic Algorithm to Path-Planning")
+    rospy.spin()
 
 
 if __name__ == "__main__":
 
-	# parser = argparse.ArgumentParser(description='Execute Genetic Algorithm to Path-Planning')
-	# parser.add_argument('integers', metavar='N', type=int, nargs='+',
-	#                     help='an integer for the accumulator')
-	# parser.add_argument('--sum', dest='accumulate', action='store_const',
-	#                     const=sum, default=max,
-	#                     help='sum the integers (default: find the max)')
+    # parser = argparse.ArgumentParser(description='Execute Genetic Algorithm to Path-Planning')
+    # parser.add_argument('integers', metavar='N', type=int, nargs='+',
+    #                     help='an integer for the accumulator')
+    # parser.add_argument('--sum', dest='accumulate', action='store_const',
+    #                     const=sum, default=max,
+    #                     help='sum the integers (default: find the max)')
 
-	# args = parser.parse_args()
+    # args = parser.parse_args()
 
     genetic_server()
